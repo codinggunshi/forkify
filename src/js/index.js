e0ba2f300f316698483cc7372f94851130b1b9cb
codinggunshi <geirinayy@gmail.com>
2020-02-19 18:37:41 -0800

    update proj, almost finish

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,14 @@ const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id
 
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
+import Likes from './models/Likes';
+
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
+import * as likesView from './views/likesView';
+
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /**  Global state of the app
@@ -27,10 +34,14 @@ import { elements, renderLoader, clearLoader } from './views/base';
  * - Liked recipes
  */
 const state = {};
+// for testing purpose
+window.state = state;
+
+
+
 
 const controlSearch = async () => {
-    // const query = searchView.getInput();
-    const query = 'pizza';
+    const query = searchView.getInput();
     console.log(query);
 
     if (query) {
@@ -86,7 +97,12 @@ const controlRecipe = async () => {
 
     if (id) {
         // prepare ui to change
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
+        // highlight selected search item
+        if (state.search) searchView.highlightSelected(id);
+        
         // create new recipe object
         state.recipe = new Recipe(id);
         // testing
@@ -94,13 +110,16 @@ const controlRecipe = async () => {
         try {
             // get recipe data
             await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
             // calculate servings and time
             state.recipe.calcTime();
             state.recipe.calcServings();
             // render recipe
-            console.log(state.recipe);
-
+            clearLoader();
+            recipeView.renderRecipe(state.recipe, state.likes.isLiked(id));
+            
         } catch(err) {
+            console.log(err);
             alert('error processing recipe')
         }
         
@@ -108,3 +127,99 @@ const controlRecipe = async () => {
 };
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+/*
+ListController
+*/
+
+const controlList = () => {
+    // create a new list if there in none yet
+    if (!state.list) state.list = new List();
+
+    // Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+}
+
+// handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+    
+    // delete button handler
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // delete from state
+        state.list.deleteItem(id);
+        // delete from UI
+        listView.deleteItem(id);
+
+    // handle the count update
+    } else if (e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+});
+
+/**
+ * Like controller
+ */
+// for quick fix
+state.likes = new Likes();
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
+
+    // User has not yet liked current recipe
+    if (!state.likes.isLiked(currentID)) {
+        // Add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img,
+        );
+        // Toggle the like button
+        likesView.toggleLikeBtn(true);
+        // Add like to UI List
+        likesView.renderLike(newLike);
+
+    // User has liked current recipe
+    } else {
+        // Remove like to the state
+        state.likes.deleteLike(currentID);
+        // Toggle the like button
+        likesView.toggleLikeBtn(false);
+        // Remove like to UI List
+        likesView.deleteLike(currentID);
+    }
+
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+}
+
+
+
+
+// handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        // decrease button is clicked
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        // increase button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+
+    } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        // Add ingredients to shopping list
+        controlList();
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        // Like controller
+        controlLike();
+    }
+});
+
+window.l = new List();
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -36,6 +36,8 @@ export default class Recipe {
         // if ounce comes before than ounces, then it only replace ounce to oz so we will have ozs
         const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
 
+        const units = [...unitsShort, 'kg', 'g'];
+
         const newIngredients = this.ingredients.map(el => {
             // 1) uniform units
             let ingredient = el.toLowerCase();
@@ -45,8 +47,68 @@ export default class Recipe {
             // 2) remove paretheses
             ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
             // 3) parse ingredients into count, unit, and ingredient
-            return ingredient;
+            const arrIng = ingredient.split(' ');
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2)); // includes is a new method, check if el2 is in the arr or not
+
+
+            let objIng;
+            if (unitIndex > -1) {
+                // there is a unit
+
+                // Example:
+                // 4  1/2 cups, arrCount is [4, 1/2]
+                //4 cups, arrCount is [4]
+                const arrCount = arrIng.slice(0, unitIndex);
+                let count;
+                if (arrCount.length === 1) {
+                    count = eval(arrCount[0].replace('-', '+')); // the only number is count
+                } else {
+                    count = eval(arrIng.slice(0, unitIndex).join('+')); // eval(4 + 1/2) => 4.5
+                }
+
+                objIng = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex + 1).join(' ')
+                }
+
+            } else if (parseInt(arrIng[0], 10)) {
+                // there is no unit, but 1st element is number
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ') // we want all ingredient except 1st num, so slice(1) to remove 0 position number
+                }
+            } else if (unitIndex === -1) {
+                // there is no unit && no number in 1st position
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredient // after es6, we can do this instead of 'ingredient : indegredient' (same name)
+                }
+            } 
+
+
+
+
+
+            return objIng;
         });
         this.ingredients = newIngredients;
     }
+
+
+    updateServings(type) {
+        // Servings
+        const newServings = type === 'dec' ? this.servings - 1 : this.servings + 1;
+        
+        
+        // Ingredients
+        this.ingredients.forEach(ing => {
+            ing.count *= (newServings / this.servings);
+        });
+
+        this.servings = newServings;
+    }
+
 }
